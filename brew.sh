11a5a8167b2748a0d89087159b3aeeb0b6e6330b
Ricco Førgaard <ricco.forgaard@apparat.no>
2019-07-22 10:08:33 +0200

    Fixes some Homebrew complanints

--- brew.sh
+++ brew.sh
@@ -5,9 +5,6 @@
 # Make sure we’re using the latest Homebrew.
 brew update
 
-# Upgrade any already-installed formulae.
-brew upgrade --all
-
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
@@ -17,12 +14,11 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
@@ -32,14 +28,12 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
 fi;
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget
 
 brew install tree
 
 # Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
+brew install vim
 
 brew install git
 
@@ -54,7 +48,6 @@ brew cask install spotify
 brew cask install slack
 brew cask install docker
 brew cask install java
-brew cask install jce-unlimited-strength-policy
 brew cask install dbeaver-community
 brew cask install ngrok
 
